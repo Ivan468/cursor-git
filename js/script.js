ac974ed602327f538f02db76e388854293c26cd6
Ivan <pig1995z@gmailcom>
2020-07-15 18:24:45 +0300

    finish

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let Test;
+Test = "hello";
+
+let a = 10,
+  b = 4;
+let c = a * (b + 4);
+console.log(c);
+
+let user = `Ivan`;
+let usertL = `Petrushchck`;
+let fullName = user + ` ` + usertL;
+console.log(fullName);
+
+let fullName1 = `${user} How are You`;
+console.log(fullName1);
+
+let userName = prompt(`What you name`);
+console.log(userName);
