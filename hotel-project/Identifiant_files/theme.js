193a3651c313f487ebf727c816a66e0b9506d6e8
Ivan <pig1995z@gmail.com>
2021-04-10 01:13:13 +0300

    add hidden password

--- hotel-project/Identifiant_files/theme.js
+++ hotel-project/Identifiant_files/theme.js
@@ -10466,3 +10466,18 @@
     n(7), (t.exports = n(8));
   },
 ]);
+
+// HIDDEN Password
+
+function viewPassword() {
+  var passwordInput = document.getElementById("password-field");
+  var passStatus = document.getElementById("pass-status");
+
+  if (passwordInput.type == "password") {
+    passwordInput.type = "text";
+    passStatus.className = "fa fa-eye-slash";
+  } else {
+    passwordInput.type = "password";
+    passStatus.className = "fa fa-eye";
+  }
+}
